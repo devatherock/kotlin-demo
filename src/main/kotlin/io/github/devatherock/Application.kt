40d05e98de538123c35b9708f352973be56fcc4f
devatherock <devatherock@gmail.com>
2024-02-08 07:00:26 -0600

    SO-77615725: Extra property within application plugin closure

---
+++ src/main/kotlin/io/github/devatherock/Application.kt
@@ -0,0 +1,11 @@
+package io.github.devatherock
+
+open class Application {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("In main function. Args: $args")
+        }
+    }
+}
